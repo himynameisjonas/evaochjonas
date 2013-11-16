7eb7aa33c3720cac87678660b40eab950aed900e
Jonas Forsberg <himynameisjonas@gmail.com>
2013-11-16 23:35:34 +0100

    preload images

--- js/app.js
+++ js/app.js
@@ -23,12 +23,17 @@ function eraseCookie(name) {
 }
 
 $(function(){
-  $('.people img').mouseenter(function(){
+  people = $('.people img');
+  people.each(function(){
+    $('<img src="'+ $(this).attr('src').replace("2", "3") +'"/>');
+  })
+
+  people.mouseenter(function(){
     img = $(this)
     img.attr('src', img.attr('src').replace("2", "3"))
   });
 
-  $('.people img').mouseleave(function(){
+  people.mouseleave(function(){
     img = $(this)
     img.attr('src', img.attr('src').replace("3", "2"))
   });
