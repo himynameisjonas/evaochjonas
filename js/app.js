10ed4cc88e0af7d483525a454e53b68bbc713b96
Jonas Forsberg <himynameisjonas@gmail.com>
2013-11-16 21:51:31 +0100

    Thank you page with gif/youtube

--- js/app.js
+++ js/app.js
@@ -1,3 +1,27 @@
+function createCookie(name, value, days) {
+    if (days) {
+        var date = new Date();
+        date.setTime(date.getTime() + (days * 24 * 60 * 60 * 1000));
+        var expires = "; expires=" + date.toGMTString();
+    } else var expires = "";
+    document.cookie = escape(name) + "=" + escape(value) + expires + "; path=/";
+}
+
+function readCookie(name) {
+    var nameEQ = escape(name) + "=";
+    var ca = document.cookie.split(';');
+    for (var i = 0; i < ca.length; i++) {
+        var c = ca[i];
+        while (c.charAt(0) == ' ') c = c.substring(1, c.length);
+        if (c.indexOf(nameEQ) == 0) return unescape(c.substring(nameEQ.length, c.length));
+    }
+    return null;
+}
+
+function eraseCookie(name) {
+    createCookie(name, "", -1);
+}
+
 $(function(){
   $('.people img').mouseenter(function(){
     img = $(this)
@@ -8,4 +32,10 @@ $(function(){
     img = $(this)
     img.attr('src', img.attr('src').replace("3", "2"))
   });
+
+  $('form').submit(function(){
+    createCookie('rsvp', $('input:radio[name=rsvp]:checked').val())
+  });
+
+  $('.rsvp-response.'+ readCookie('rsvp')).show();
 })
