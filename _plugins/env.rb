134b20b924ffcec610bc2eff1bb675ffe3a2493b
Jonas Forsberg <himynameisjonas@gmail.com>
2014-04-17 08:47:04 +0200

    Analytics

--- _plugins/env.rb
+++ _plugins/env.rb
@@ -2,6 +2,7 @@ module Jekyll
   class EnvironmentVariablesGenerator < Generator
     def generate(site)
       site.config['simpleform_api_key'] = ENV['SIMPLEFORM_API_KEY'] || 'xxx'
+      site.config['google_analytics'] = ENV['GOOGLE_ANALYTICS'] || 'xxx'
     end
   end
 end
